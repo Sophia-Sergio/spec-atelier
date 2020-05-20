06695dae832ec51aa00ce96a1790f4fd1bc7c45b
Sophia-Sergio <san.storres@gmail.com>
2020-05-19 22:52:10 -0400

    Add new mapping for project types

--- app/controllers/api/configs_controller.rb
+++ app/controllers/api/configs_controller.rb
@@ -6,7 +6,7 @@ module Api
       cities = CITIES.values.flatten
       data = {
         cities: cities,
-        project_types: project_types
+        project_types: project_types,
         work_types: work_types,
         room_types: room_types
       }
