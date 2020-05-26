e0582dbb3c8faa72b5910a0dc4698beca60085ad
Sophia-Sergio <san.storres@gmail.com>
2020-05-25 22:45:17 -0400

    Change project types

--- db/migrate/20200526022952_change_seeded_project_type.rb
+++ db/migrate/20200526022952_change_seeded_project_type.rb
@@ -1,8 +1,8 @@
 class ChangeSeededProjectType < ActiveRecord::Migration[6.0]
   def change
-    row = LookUpTable.find_by(category: 'project_type', value: 'commercial')
+    row = LookupTable.find_by(category: 'project_type', value: 'commercial')
     row&.update(value: 'hotel', translation_spa: 'hotel')
 
-    LookUpTable.find_by(category: 'project_type', value: 'office')&.delete
+    LookupTable.find_by(category: 'project_type', value: 'office')&.delete
   end
 end
