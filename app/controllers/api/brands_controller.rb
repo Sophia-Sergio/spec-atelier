0098a8479f6a1611f0787a963f8adfcdb0722fc7
Sophia-Sergio <san.storres@gmail.com>
2020-05-29 03:12:24 -0400

    Add new contact form

--- app/controllers/api/brands_controller.rb
+++ app/controllers/api/brands_controller.rb
@@ -1,17 +1,35 @@
 module Api
   class BrandsController < ApplicationController
+    include Search::Handler
     before_action :valid_session
 
     def index
-      list = Brand.all
-      list = list.map {|brand| { id: brand.id, name: brand.name } }
-      render json: { brands: list }, status: :ok
+      decorated_list = presenter.decorate_list(filtered_list.includes(:products), params)
+      render json: { brands: decorated_list }, status: :ok
     end
 
-    def search
-      list = Brand.search(params[:query]).limit(10)
-      list = list.map {|brand| { id: brand.id, name: brand.name } }
-      render json: { brands: list }, status: :ok
+    def show
+      render json: { brand: presenter.decorate(brand) }, status: :ok
+    end
+
+    def contact_form
+      contact_form = brand.brand_contact_forms.create(contact_form_params.merge(user_id: current_user.id))
+      BrandMailer.send_contact_form(current_user, contact_form).deliver
+      render json: { brand: contact_form, message: 'Mensaje enviado'}, status: :created
+    end
+
+    private
+
+    def brand
+      Brand.find(params[:id] || params[:brand_id])
+    end
+
+    def presenter
+      Brands::BrandPresenter
+    end
+
+    def contact_form_params
+      params.permit(:user_phone, :message)
     end
   end
 end
