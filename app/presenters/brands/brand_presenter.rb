25328488770ee8c3f8a937efbfb191408cadf624
Sophia-Sergio <san.storres@gmail.com>
2020-06-03 22:28:31 -0400

    Add desc and product_images

--- app/presenters/brands/brand_presenter.rb
+++ app/presenters/brands/brand_presenter.rb
@@ -1,9 +1,13 @@
 module Brands
   class BrandPresenter < Presenter
-    will_print :id, :name, :products_count, :description, :address, :country, :phone, :web, :email, :social_media
+    will_print :id, :name, :products_count, :description, :address, :country, :phone, :web, :email, :social_media :product_images
 
     def products_count
       brand.products.count
+    end
+
+    def description
+      "<p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui</p>"
     end
 
     def image
