03b64a8e9b23caa75764fd1507165ce931500d8b
Sophia-Sergio <san.storres@gmail.com>
2020-07-03 18:50:21 -0400

    Refactor images, documents and brand foreign key

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,24 +2,36 @@ class Product < ApplicationRecord
   include MetaLookupTable
   include PgSearch::Model
 
-  belongs_to :brand
+  belongs_to :brand, foreign_key: 'company_id', class_name: 'Company::Brand'
   belongs_to :subitem, optional: true
   belongs_to :item
   has_one :section, through: :item
-  has_many :images, as: :owner, class_name: 'Attached::Image', dependent: :destroy
-  has_many :documents, as: :owner, class_name: 'Attached::Document', dependent: :destroy
-
-  validates :name, :brand, :item, :long_desc, :price, presence: true
+  has_many :files, as: :owner, class_name: 'Attached::ResourceFile'
+  validates :name, :brand, :item, :long_desc, presence: true
 
   pg_search_scope :by_keyword,
     against: %i[name short_desc long_desc reference],
     associated_against: { brand: :name },
     using: { tsearch: { prefix: true, any_word: true } }
 
-  scope :by_brand,        ->(brands)   { joins(:brand).where(brands: { id: brands }) }
+  scope :by_brand,        ->(brands)   { joins(:brand).where(companies: { id: brands }) }
   scope :by_section,      ->(sections) { joins(:section).where(sections: { id: sections }) }
   scope :by_item,         ->(items)    { joins(:item).where(items: { id: items }) }
   scope :by_project_type, ->(types)    { where("project_type && ?", "{#{ types.is_a?(Array) ? types.join(',') : types }}") }
   scope :by_room_type,    ->(types)    { where("room_type && ?", "{#{ types.is_a?(Array) ? types.join(',') : types }}") }
+
+  before_validation(on: :create) do
+    self.item = subitem.item if subitem.present?
+  end
+
+  def images
+    images = files.images&.pluck(:attached_file_id)
+    Attached::Image.where(id: images).joins(:resource_file).order('attached_resource_files.order')
+  end
+
+  def documents
+    documents = files.documents&.pluck(:attached_file_id)
+    Attached::Document.where(id: documents).joins(:resource_file).order('attached_resource_files.order')
+  end
 
 end
