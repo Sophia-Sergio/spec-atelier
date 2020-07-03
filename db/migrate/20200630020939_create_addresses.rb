f30cb6d838a35ed94bf43cf9fd6cd415f3411ca2
Sophia-Sergio <san.storres@gmail.com>
2020-07-03 19:13:20 -0400

    Add address model and table

---
+++ db/migrate/20200630020939_create_addresses.rb
@@ -0,0 +1,13 @@
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.string :name
+      t.string :text
+      t.string :country
+      t.string :city
+      t.references  :owner, polymorphic: true,  null: true
+      t.integer     :order, default: 0
+      t.timestamps
+    end
+  end
+end
