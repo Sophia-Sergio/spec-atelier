2dd2b774e30b066e64dffc9386cfd7e36156d1d0
Sophia-Sergio <san.storres@gmail.com>
2020-07-13 23:49:02 -0400

    Add mocked key data

--- spec/factories/lookup_tables.rb
+++ spec/factories/lookup_tables.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :lookup_table do
     sequence(:code)     {|n| n}
     sequence(:value)    {|n| "value #{n}"}
+    sequence(:translation_spa)    {|n| "translation #{n}"}
   end
 end
