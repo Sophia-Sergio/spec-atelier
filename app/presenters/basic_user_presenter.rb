d7d8f6ab04835e85c28e7ddc5c9cfd0a13ffdf11
Sophia-Sergio <san.storres@gmail.com>
2020-07-13 23:48:09 -0400

    Add projects count to user show

--- app/presenters/basic_user_presenter.rb
+++ app/presenters/basic_user_presenter.rb
@@ -1,7 +1,11 @@
 class BasicUserPresenter < Presenter
-  will_print :id, :email, :jwt, :first_name, :last_name, :birthday, :office, :profile_image
+  will_print :id, :email, :jwt, :first_name, :last_name, :birthday, :office, :profile_image, :projects_count
 
   def jwt
     subject.session.token rescue 'not logged in'
+  end
+
+  def projects_count
+    subject.projects.count
   end
 end
