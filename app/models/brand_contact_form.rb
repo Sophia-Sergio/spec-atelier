0e71c78f330cdc076e95514b5f767e9f11c75284
Sophia-Sergio <san.storres@gmail.com>
2020-07-13 23:26:40 -0400

    Remove unused files

--- app/models/brand_contact_form.rb
+++
@@ -1,4 +0,0 @@
-class BrandContactForm < ApplicationRecord
-  belongs_to :brand, class_name: 'Company::Brand', foreign_key: 'company_id'
-  belongs_to :user
-end
--- db/migrate/20200518021432_add_fields_to_project_spec_item.rb
+++
@@ -1,6 +0,0 @@
-class AddFieldsToProjectSpecItem < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :project_spec_items, :section, null: false, foreign_key: true
-    add_reference :project_spec_items, :item, null: false, foreign_key: true
-  end
-end
