815f0048fc89e453c885c522f6a6acd75832df86
Sophia-Sergio <san.storres@gmail.com>
2020-07-14 00:23:49 -0400

    Change name of migration

--- db/migrate/20200529064706_create_contact_forms.rb
+++ db/migrate/20200529064706_create_contact_forms.rb
@@ -1,4 +1,4 @@
-class CreateBrandContactForms < ActiveRecord::Migration[6.0]
+class CreateContactForms < ActiveRecord::Migration[6.0]
   def change
     create_table :contact_forms do |t|
       t.references :owner, polymorphic: true
