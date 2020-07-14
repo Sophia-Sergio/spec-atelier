babb9de0644bfa36308057324e8fcb104d8878d0
Sophia-Sergio <san.storres@gmail.com>
2020-07-13 23:46:06 -0400

    Product mailer

---
+++ app/mailers/product_mailer.rb
@@ -0,0 +1,20 @@
+class ProductMailer < ApplicationMailer
+  default from: 'paul.eaton@specatelier.com'
+
+  def send_contact_form_to_brand(current_user, form)
+    @current_user = current_user
+    @form = form
+    @product = @form.owner
+    @brand = @product.brand
+    @brand_email = @brand.email['main']
+    mail(to: @brand_email, subject: 'Product contact_form')
+  end
+
+  def send_contact_form_to_user(current_user, form)
+    @current_user = current_user
+    @form = form
+    @product = @form.owner
+    @brand = @product.brand
+    mail(to: @current_user.email, subject: 'Your mail was sent contact_form')
+  end
+end
---
+++ db/migrate/20200529064706_create_contact_forms.rb
@@ -0,0 +1,13 @@
+class CreateBrandContactForms < ActiveRecord::Migration[6.0]
+  def change
+    create_table :contact_forms do |t|
+      t.references :owner, polymorphic: true
+      t.references :user, null: false, foreign_key: true
+      t.string :user_phone
+      t.string :message
+      t.string :type
+
+      t.timestamps
+    end
+  end
+end
