2b5adad62537e47816b63ea4e1e9032a0e39da64
Ibox <jonathan.araya.m@gmail.com>
2020-07-15 07:35:25 -0400

    [email] test

--- app/controllers/api/brands_controller.rb
+++ app/controllers/api/brands_controller.rb
@@ -14,9 +14,10 @@ module Api
 
     def contact_form
       contact_form = brand.contact_forms.create(contact_form_params.merge(user_id: current_user.id))
-      BrandMailer.send_contact_form_to_brand(current_user, contact_form).deliver
-      BrandMailer.send_contact_form_to_user(current_user, contact_form).deliver
-      render json: { form: contact_form, message: 'Mensaje enviado' }, status: :created
+      product = product.contact_forms.create(contact_form_params.merge(user_id: current_user.id))
+      BrandMailer.send_contact_form_to_brand(current_user, contact_form, product).deliver
+      BrandMailer.send_contact_form_to_user(current_user, contact_form, product).deliver
+      render json: { form: contact_form, product: product, message: 'Mensaje enviado' }, status: :created
     end
 
     private
--- app/mailers/brand_mailer.rb
+++ app/mailers/brand_mailer.rb
@@ -1,18 +1,20 @@
 class BrandMailer < ApplicationMailer
   default from: 'paul.eaton@specatelier.com'
 
-  def send_contact_form_to_brand(current_user, form)
+  def send_contact_form_to_brand(current_user, form, product)
     @current_user = current_user
     @form = form
     @brand = @form.owner
     @brand_email = @brand.email['main'] || 'jonathan.araya.m@gmail.com'
+    @product = product
     mail(to: @brand_email, subject: 'Brand contact_form')
     mail(to: 'jonathan.araya.m@gmail.com', subject: 'Brand contact_form')
   end
 
-  def send_contact_form_to_user(current_user, form)
+  def send_contact_form_to_user(current_user, form, product)
     @current_user = current_user
     @brand = form.owner
+    @product = product
     mail(to: @current_user.email, subject: 'Your mail was sent contact_form')
   end
 end
