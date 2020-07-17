feecb5fb7896f40f13e6de975fd49c1564055fb7
Sophia-Sergio <san.storres@gmail.com>
2020-07-16 22:05:26 -0400

    Add project_spec_id to json response

--- app/presenters/projects/private_project_presenter.rb
+++ app/presenters/projects/private_project_presenter.rb
@@ -1,5 +1,9 @@
 module Projects
   class PrivateProjectPresenter < Presenter
-    will_print :id, :name, :project_type, :work_type, :country, :city, :delivery_date, :status, :created_at, :updated_at
+    will_print :id, :name, :project_type, :work_type, :country, :city, :delivery_date, :status, :project_spec_id, :created_at, :updated_at,
+
+    def project_spec_id
+      subject.specification.id
+    end
   end
 end
