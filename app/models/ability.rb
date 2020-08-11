1bb094676afb0d94913f64f9aac67751a83ac7f7
Sophia-Sergio <san.storres@gmail.com>
2020-08-10 22:46:44 -0400

    Add ability for user to edit product

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,11 @@ class Ability
 
   def initialize(current_user)
     can :show, User
+
+    can :update, Product do |product|
+      can :update, Product if product.user ==  current_user
+    end
+
     if current_user.superadmin?
       can :update, User
     elsif current_user.user?
