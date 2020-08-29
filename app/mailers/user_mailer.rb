7420c47fbd47b57397a460a98ec01b8e89d725d5
Ibox <jonathan.araya.m@gmail.com>
2020-08-29 14:32:51 -0400

    [emails]

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,17 +4,15 @@ class UserMailer < ApplicationMailer
   def send_signup_email(user)
     @user = user
     mail(to: user.email, subject: 'Thanks for signing up for our amazing app')
-    mail(to: 'jonathan.araya.m@gmail.com', subject: 'Thanks for signing up for our amazing app')
   end
 
   def password_reset(user)
     @user = user
-    mail(to: 'san.storres@gmail.com', subject: 'Reset password')
+    mail(to: user.email, subject: 'Reset password')
   end
 
   def password_reset_success(user)
     @user = user
     mail(to: user.email, subject: 'New Password')
-    mail(to: 'jonathan.araya.m@gmail.com', subject: 'New password')
   end
 end
