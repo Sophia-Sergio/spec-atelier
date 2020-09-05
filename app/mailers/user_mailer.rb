97ea08390481742ff1fd42ebf82666e8d9f0a729
Sophia-Sergio <san.storres@gmail.com>
2020-09-05 16:30:24 -0400

    Add workers for handling mailing

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,16 +2,16 @@ class UserMailer < ApplicationMailer
 
   def send_signup_email(user)
     @user = user
-    mail(to: user.email, subject: 'Thanks for signing up for our amazing app')
+    mail(to: @user.email, subject: 'Thanks for signing up for our amazing app')
   end
 
   def password_reset(user)
     @user = user
-    mail(to: user.email, subject: 'Reset password')
+    mail(to: @user.email, subject: 'Reset password')
   end
 
   def password_reset_success(user)
     @user = user
-    mail(to: user.email, subject: 'New Password')
+    mail(to: @user.email, subject: 'New Password')
   end
 end
