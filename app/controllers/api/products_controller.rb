95939064b5e37c401153fe821d8ae35a4e97c094
Sophia-Sergio <san.storres@gmail.com>
2020-09-07 23:28:34 -0300

    Allow index for all users

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,7 +3,7 @@ module Api
     include Search::Handler
     include AssociateFiles
 
-    before_action :valid_session, except: :send_email
+    before_action :valid_session, except: %i[send_email index]
     before_action :product, only: %i[show]
 
     load_and_authorize_resource only: %i[update]
