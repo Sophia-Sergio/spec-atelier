f30a49fc8e42f6daa1aec598c8d84f884eb1207e
Sergio Torres <san.storres@gmail.com>
2020-11-25 00:35:47 -0300

    Add rfa en vez de bim para bim url

--- app/models/attached/document.rb
+++ app/models/attached/document.rb
@@ -1,7 +1,7 @@
 module Attached
   class Document < Attached::File
     scope :with_dwg, -> { by_extension('dwg') }
-    scope :with_bim, -> { by_extension('bim') }
+    scope :with_bim, -> { by_extension('rfa') }
     scope :with_pdf, -> { by_extension('pdf') }
 
   end
