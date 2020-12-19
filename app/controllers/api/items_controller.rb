0acb0a3815fa1a45fab7d68d26e6f973ed693fe5
Sergio Torres <san.storres@gmail.com>
2020-12-19 11:42:27 -0300

    fix name of param

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -16,8 +16,8 @@ module Api
 
     def index
       list = Item.all
+      list = list.where(section_id: params[:section_id]) if params[:section_id].present?
       list = list.with_products if params[:with_products]
-      list = list.where(section_id: params[:section]) if params[:section].present?
       list = list.order(:name).map {|item| { id: item.id, name: item.name } }
       render json: { items: list }
     end
