e6811ef613bb98d1679a807079ccc76321bc718d
Sergio Torres <san.storres@gmail.com>
2020-12-20 15:05:04 -0300

    style refactor

--- config/initializers/open_classes.rb
+++ config/initializers/open_classes.rb
@@ -11,20 +11,19 @@ module ActiveRecordUnion
     def union(*relations)
       raise ArgumentError, "wrong number of arguments (given 0, expected 1+)" if relations.empty?
 
-      valid_relations = relations
-        .select do |relation|
-          raise ArgumentError, "type mismatch. Base model table #{table_name} does not match table #{relation.table_name} of at least one relation" if table_name != relation.table_name
+      valid_relations = relations.select do |relation|
+        raise_type_mismatch_error if table_name != relation.table_name
+        relation.to_sql.present?
+      end
 
-          relation.to_sql.present?
-        end
-
-      mapped_sql = valid_relations
-        .map(&:to_sql)
-        .join(") UNION (")
-
+      mapped_sql = valid_relations.map(&:to_sql).join(") UNION (")
       unionized_sql = "((#{mapped_sql})) #{table_name}"
 
       where(id: from(unionized_sql))
+    end
+
+    def raise_type_mismatch_error
+      raise ArgumentError, "type mismatch. Base model table #{table_name} does not match table #{relation.table_name} of at least one relation"
     end
   end
 end
