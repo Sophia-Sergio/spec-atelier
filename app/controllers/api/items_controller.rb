361adaf074dc905aa0a27f7b8732ec138f804447
Sergio Torres <san.storres@gmail.com>
2020-12-26 15:49:56 -0300

    use section instead of section_id

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -16,7 +16,7 @@ module Api
 
     def index
       list = Item.all
-      list = list.where(section_id: params[:section_id]) if params[:section_id].present?
+      list = list.where(section: params[:section]) if params[:section].present?
       list = list.with_products if params[:with_products]
       list = list.order(:name).map {|item| { id: item.id, name: item.name } }
       render json: { items: list }
