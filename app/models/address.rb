19b10a4a16f0360987754e9609d804f07fc5c74e
Sergio Torres <san.storres@gmail.com>
2021-03-17 00:54:24 -0300

    address shold have owner

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,5 +2,5 @@ class Address < ApplicationRecord
   validates :text, presence: true
   validates :country, presence: true
   validates :city, presence: true
-  belongs_to :owner, polymorphic: true, optional: true
+  belongs_to :owner, polymorphic: true
 end
