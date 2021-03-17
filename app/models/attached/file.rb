ba2f197544fa82fe702ea302910df763df65cbda
Sergio Torres <san.storres@gmail.com>
2021-03-17 00:55:05 -0300

    add dependent: :destroy

--- app/models/attached/file.rb
+++ app/models/attached/file.rb
@@ -5,8 +5,8 @@ module Attached
     end
 
     validates :name, presence: true
-    scope :by_extension, ->(extension) { where('name LIKE ?', "%.#{extension}%")}
-    has_one :resource_file, class_name: 'Attached::ResourceFile', foreign_key: :attached_file_id
+    scope :by_extension, ->(extension) { where('name LIKE ?', "%.#{extension}%") }
+    has_one :resource_file, class_name: 'Attached::ResourceFile', foreign_key: :attached_file_id, dependent: :destroy
 
     %w[image document].each do |e|
       define_method("#{e}?") { type.eql? "Attached::#{e.capitalize}" }
