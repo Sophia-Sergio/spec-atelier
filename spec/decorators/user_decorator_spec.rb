05e67c8f631a730b89e7d876220a9f654caaa2e9
Sergio Torres <san.storres@gmail.com>
2021-03-28 22:22:44 -0300

    feat: add clients to user

---
+++ spec/decorators/user_decorator_spec.rb
@@ -0,0 +1,11 @@
+RSpec.describe UserDecorator do
+  let(:user) { create(:user) }
+  let(:decorator) { UserDecorator.decorate(user) }
+
+  USER_EXPECTED_KEYS = %w[id email jwt first_name last_name profile_image projects_count city company client?].freeze
+
+  it 'has proper keys' do
+    decorator_json = decorator.as_json
+    expect(decorator_json.keys).to match_array(USER_EXPECTED_KEYS)
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,20 @@ describe User, type: :model do
       expect(user.name).to be nil
     end
   end
+
+  describe '#client?' do
+    context 'when it has a client role' do
+      before { user.add_role(:client) }
+
+      it 'return true' do
+        expect(user.client?).to be true
+      end
+    end
+
+    context 'when it hasnt a client role' do
+      it 'return true' do
+        expect(user.client?).to be false
+      end
+    end
+  end
 end
