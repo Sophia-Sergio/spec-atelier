42189f5ef0e10f86152ac8bc129865a8748831fd
Sergio Torres <san.storres@gmail.com>
2021-04-04 22:27:31 -0400

    test: add test for stats update

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,10 +2,44 @@ describe Product, type: :model do
   let(:product) { create(:product) }
 
   describe 'factory built record' do
-    it 'atteches a file' do
+    it 'attaches a file' do
       image = create(:image)
       create(:resource_file, owner: product, attached: image)
       expect(product.images.length).to be(1)
+    end
+  end
+
+  describe '#used_on_spec_stat_update' do
+    context 'if product_stat does not exists' do
+      it 'created a record on stats' do
+        create(:product, :used_on_spec, original_product_id: product.id)
+        expect(product.reload.stats_used_on_spec).to be 1
+      end
+    end
+
+    context 'if product_stat does exists' do
+      it 'created a record on stats' do
+        create(:product, :used_on_spec, original_product_id: product.id)
+        create(:product, :used_on_spec, original_product_id: product.id)
+        expect(product.reload.stats_used_on_spec).to be 2
+      end
+    end
+  end
+
+  describe '#used_on_spec_stat_update' do
+    it 'updated the counter -1' do
+      product_spec = create(:product, :used_on_spec, original_product_id: product.id)
+      expect(product.reload.stats_used_on_spec).to be 1
+      product_spec.destroy
+      expect(product.reload.stats_used_on_spec).to be 0
+    end
+  end
+
+  describe '#destroy' do
+    it 'destroys product stats' do
+      create(:product, :used_on_spec, original_product_id: product.id)
+      product.destroy
+      expect(ProductStat.find_by(product: product).present?).to be false
     end
   end
 
