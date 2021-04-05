b97dafe970999d2657b2aee09ac20e1a65770450
Sergio Torres <san.storres@gmail.com>
2021-04-04 22:34:02 -0400

    feat: add way to pagiunated actoverecord

--- config/initializers/open_classes.rb
+++ config/initializers/open_classes.rb
@@ -22,6 +22,28 @@ module ActiveRecordUnion
       where(id: from(unionized_sql))
     end
 
+    def paginated_format(decorator, params = {})
+      params[:page] = params[:page].presence&.to_i || 0
+      params[:offset] = params[:offset].presence&.to_i || params[:limit].presence&.to_i || 10
+      params[:limit] = params[:limit].presence&.to_i || 10
+      list = all
+      {
+        total: list.count,
+        list: paginated_list(list, decorator, params),
+        next_page: (params[:page] + 1) * params[:limit] < list.count ? params[:page] + 1 : nil
+      }
+    end
+
+    private
+
+    def paginated_list(list, decorator, params)
+      ordered = list.pluck(:id)
+      decorator.decorate_collection(
+        list.offset(params[:limit] * params[:page]).limit(params[:limit]).find_ordered(ordered),
+        context: params
+      )
+    end
+
     def raise_type_mismatch_error
       raise ArgumentError, "type mismatch. Base model table #{table_name} does not match table #{relation.table_name} of at least one relation"
     end
