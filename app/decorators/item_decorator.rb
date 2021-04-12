0667e7480af87ead1b0cffddcf5f36b478725ec9
Sergio Torres <san.storres@gmail.com>
2021-04-11 20:27:17 -0400

    feat: add subitems to item index response

--- app/decorators/item_decorator.rb
+++ app/decorators/item_decorator.rb
@@ -1,3 +1,8 @@
 class ItemDecorator < ApplicationDecorator
   delegate :id, :name, :show_order, :code
+  new_keys :subitems
+
+  def subitems
+    model.subitems.with_products.map {|subitem| { id: subitem.id, name: subitem.name } }
+  end
 end
