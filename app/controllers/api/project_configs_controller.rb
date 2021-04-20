f7d82499bc6259839ab7d9723578a487d446e4fe
Sergio Torres <san.storres@gmail.com>
2021-04-20 01:36:37 -0400

    feat: create action project_configs

---
+++ app/controllers/api/project_configs_controller.rb
@@ -0,0 +1,19 @@
+module Api
+  class ProjectConfigsController < ApplicationController
+    before_action :valid_session
+
+    def create
+      config = Project.find(params[:project_id]).config
+      config.update!(project_config_params)
+      render json: { project_config: config.as_json(except: %i[created_at updated_at id])}
+    end
+
+    private
+
+    def project_config_params
+      params.require(:project_config).permit(
+        visible_attrs: { product: %i[all brand long_desc short_desc reference] }
+      )
+    end
+  end
+end
