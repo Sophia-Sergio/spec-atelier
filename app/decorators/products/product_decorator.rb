9fc9734a806485c09113918670c2d4cecfd71b9a
Sergio Torres <san.storres@gmail.com>
2021-04-25 23:53:48 -0400

    feat: add user_owned to products

--- app/decorators/products/product_decorator.rb
+++ app/decorators/products/product_decorator.rb
@@ -1,6 +1,6 @@
 module Products
   class ProductDecorator < ApplicationDecorator
-    delegate :id, :short_desc, :long_desc, :reference, :price, :original_product_id
+    delegate :id, :short_desc, :long_desc, :reference, :price, :original_product_id; :name
     new_keys :name,
              :system,
              :systems,
@@ -19,14 +19,6 @@ module Products
              :work_type,
              :user_owned
 
-    def name
-      if block.present?
-        "#{block.section_order}.#{block.item_order}.#{block.product_order}. #{model.name}"
-      else
-        model.name
-      end
-    end
-
     def brand
       resource = model.brand
       { id: resource&.id, name: resource&.name }
@@ -37,16 +29,8 @@ module Products
       { id: resource&.id, name: resource&.name }
     end
 
-    def system
-      { id: subitem_object.id, name: subitem_object.name } if subitem_object.present?
-    end
-
     def systems
       model.subitems.map {|subitem| { id: subitem&.id, name: subitem&.name } }
-    end
-
-    def section
-      { id: section_object.id, name: section_object.name } if section_object.present?
     end
 
     def sections
@@ -92,7 +76,7 @@ module Products
     end
 
     def user_owned
-      model.original_product.user == model.user
+      user == model.user
     end
 
     %w[project work room].each do |column|
@@ -101,36 +85,16 @@ module Products
 
     private
 
-    def block
-      @block ||= context[:block].presence
+    def user
+      @user ||= context[:user].presence
     end
 
     def documents
       @documents ||= model.documents
     end
 
-    def section_object
-      @section_object ||= if block.present?
-        model.spec_item&.section
-      else
-        model.sections.first
-      end
-    end
-
-    def item_object
-      @item_object ||= if block.present?
-        model.spec_item
-      else
-        model.items.find_by(id: context["item"] || subitem_object&.item_id)
-      end
-    end
-
-    def subitem_object
-      @subitem_object ||= model.subitems.find_by(id: context["subitem"])
-    end
-
     def item_image
-      item = item_object.present? ? item_object : model.items.first
+      item = model.items.first
       { id: 1, hide_delete: true, urls: { small: item.image_url || '', medium: item.image_url || '' }, order: 0 }
     end
   end
