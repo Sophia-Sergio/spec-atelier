a2893a82f61f622a619814e47c832c49610af2fe
Sergio Torres <san.storres@gmail.com>
2021-05-04 22:49:41 -0400

    fix: use current_user

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -12,7 +12,7 @@ module Api
       list = Item.all.order(:show_order)
       list = list.where(section: params[:section]) if params[:section].present?
       list = list.with_products if params[:with_products]
-      render json: { items: ItemDecorator.decorate_collection(list, context: { user: user }) }
+      render json: { items: ItemDecorator.decorate_collection(list, context: { user: current_user }) }
     end
 
     private
