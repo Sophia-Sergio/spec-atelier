adc95f3e0e24846058bcd36e05a0dc9f040b6eb6
Sergio Torres <san.storres@gmail.com>
2021-05-04 22:54:36 -0400

    fix: add distinct

--- app/models/subitem.rb
+++ app/models/subitem.rb
@@ -3,11 +3,11 @@ class Subitem < ApplicationRecord
   has_many :product_subitems
   has_many :products, through: :product_subitems, dependent: :destroy
 
-  scope :with_products, -> { joins(:products).where(products: { original_product_id: nil }) }
+  scope :with_products, -> { joins(:products).where(products: { original_product_id: nil }).distinct }
   scope :with_accessible_products, -> (user) {
     ability = Abilities::ProductAbility.new(user)
     products = Product.accessible_by(ability).select(:id)
-    joins(:products).where(products: { id: products, original_product_id: nil })
+    joins(:products).where(products: { id: products, original_product_id: nil }).distinct
   }
 
 end
