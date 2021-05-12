40c251d042e5783764ed9cd9754f9d9389f4704c
Sergio Torres <san.storres@gmail.com>
2021-05-11 21:35:15 -0400

    feat: add original format

--- app/models/attached/image.rb
+++ app/models/attached/image.rb
@@ -4,6 +4,7 @@ module Attached
 
     def all_formats
       FORMATS.each_with_object({}) {|key, h| h[key] = "#{url.gsub(name, '')}resized-#{key}-#{name}" }
+             .merge(original: url)
     end
 
     FORMATS.each {|format| define_method("#{format}_format") { all_formats[format] } }
